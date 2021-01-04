a8ab24125568982e8ac080053a46257f4a85176f
PabloCh95 <58525405+PabloCh95@users.noreply.github.com>
2021-01-04 15:28:24 -0300

    modificacion del api

--- src/api/config.js
+++ src/api/config.js
@@ -1,2 +1,3 @@
-export const basePath = "https://backend-sendmail.herokuapp.com/api"
+export const basePath = "https://www.benitezpinturas.com.ar/api";
 //"http://localhost:3000/api"
+//"https://www.benitezpinturas.com.ar/api"
--- src/api/sendmail.js
+++ src/api/sendmail.js
@@ -1,7 +1,7 @@
 import { basePath } from "./config"
 
 export function sendMailApi(nombre, telefono, email, mensaje, humanKey) {
-  const url = `${basePath}/send-mail`
+  const url = `${basePath}/send-mail`;
   const params = {
     method: "POST",
     headers: {
@@ -22,11 +22,11 @@ export function sendMailApi(nombre, telefono, email, mensaje, humanKey) {
     })
 }
 /*Error : Access to XMLHttpRequest at 'http://localhost:63855/socket.io/?EIO=3&transport=polling&t=NIjgir9'
- from origin 'http://localhost:8000' has been blocked by CORS policy: 
- The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*' 
+ from origin 'http://localhost:8000' has been blocked by CORS policy:
+ The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*'
  when the request's credentials mode is 'include'.
 The credentials mode of requests initiated by the XMLHttpRequest is controlled by the withCredentials attribute.*/
 
-/*Otro error: Access to fetch at 'https://backend-sendmail.herokuapp.com/api/send-mail' 
-from origin 'http://localhost:8000' has been blocked by CORS policy: 
+/*Otro error: Access to fetch at 'https://backend-sendmail.herokuapp.com/api/send-mail'
+from origin 'http://localhost:8000' has been blocked by CORS policy:
 Response to preflight request doesn't pass access control check: It does not have HTTP ok status.*/
